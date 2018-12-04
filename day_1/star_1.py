55260049c78320bb6bf3fe7d5d68ec85a57e213a
Drew Taylor <dretaylo@cisco.com>
2018-12-03 20:15:36 -0800

    Starting day 2

---
+++ day_1/star_1.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import requests
+
+list_of_freqs = []
+
+with open('input.txt', 'rb') as f:
+    for line in f:
+        list_of_freqs.append(int(line.strip()))
+
+def total_sum(frequencies):
+    total = 0
+    for freq in frequencies:
+        total += freq
+    return total
+
+def check_for_sum(frequencies):
+    total = 0
+    sum_frequencies = [0]
+    while True:
+        for freq in frequencies:
+            total += freq
+            if total not in sum_frequencies:
+                sum_frequencies.append(total)
+            else:
+                return total
+
+print(total_sum(list_of_freqs))
+print(check_for_sum(list_of_freqs))
